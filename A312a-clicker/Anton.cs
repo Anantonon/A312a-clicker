0ee9a2afc0bc9bba99b611275e9cf0ec4ad3cf28
Lasse Østergaard <last16@student.aau.dk>
2017-04-03 12:41:08 +0200

    Changed Anton.Price to increase faster

--- A312a-clicker/Anton.cs
+++ A312a-clicker/Anton.cs
@@ -13,7 +13,7 @@ namespace A312a_clicker
         public int Number = 0;
         public double Price()
         {
-            return 1.6 * (2 * Number);
+            return 1.6 * (2 * Math.Pow(Number, 2));
         }
     }
 }
